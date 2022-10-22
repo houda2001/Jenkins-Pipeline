c5885085755db0da806f2e38afb2087307264e58
houda2001 <81920037+houda2001@users.noreply.github.com>
2022-10-22 15:55:28 +0100

    Jenkins Pipeline

---
+++ CI/CD Pipeline/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express()
+app.get("/", (req,res)=>{
+    res.send("<h2>Hi I'm Houda Karam</h2>")
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>console.log(`Listening on port ${port}`))
